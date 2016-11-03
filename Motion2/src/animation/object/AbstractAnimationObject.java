01e9f89ed54df85c1191d95ab7540a85f10cb7ce
Radonja <rudonja8@gmail.com>
2016-11-03 21:27:13 +0100

    Home, new animation

---
+++ Motion2/src/animation/object/AbstractAnimationObject.java
@@ -0,0 +1,34 @@
+package animation.object;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public abstract class AbstractAnimationObject {
+
+	protected Integer x;
+	protected Integer y;
+	
+	protected Color color;
+	
+	public AbstractAnimationObject(Integer x, Integer y) {
+		this.x = x;
+		this.y = y;
+		this.color = Color.black;
+	}
+	
+	// Method for moving object coordinates
+	public abstract void move(Integer x, Integer y);
+	
+	// Method for drawing object
+	public abstract void draw(Graphics2D g);
+	
+	public Color getColor() {
+		return color;
+	}
+	
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	
+}

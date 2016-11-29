2d8403437ebd9abc32202afb1b4b437b5c8a516a
Radonja <rudonja8@gmail.com>
2016-11-29 21:27:57 +0100

    home: Project initialization. Created first example. Animations can work

---
+++ Physics/src/helpers/Constants.java
@@ -0,0 +1,20 @@
+package helpers;
+
+public class Constants {
+	
+	// Panel properties
+	public static Integer PANEL_ANIMATION_WIDTH = 700;
+	public static Integer PANEL_ANIMATION_HEIGHT = 500;
+	
+	//Frame properties
+	public static Integer FRAME_WIDTH = PANEL_ANIMATION_WIDTH + 100;
+	public static Integer FRAME_HEIGHT = PANEL_ANIMATION_HEIGHT + 100;
+	
+	// Animation properties
+	public static Integer SIMULATION_SLEEP_TIME = 10;
+	public static Double OBJECT_ACCELERATION = 0.01;
+	public static Double OBJECT_VELOCITY = 1.0;
+	public static Double VELOCITY_LIMITATION = 4.5;
+	
+	
+}
